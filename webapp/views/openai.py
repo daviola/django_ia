5e7e44d884c92854c5c5d2275cad184a8b738e5c
daviola <18071455+daviola@users.noreply.github.com>
2023-03-31 14:43:14 -0300

    adiciona titulo

--- webapp/views/openai.py
+++ webapp/views/openai.py
@@ -3,4 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 
 def correcao(request):
-    return render(request, "correcao.html", {"teste": "teste123"})
+    params = {
+        "view":{
+            "codigo": "correcao",
+            "titulo": "Correção de Código"
+        }
+    }
+    return render(request, "correcao.html", params)
