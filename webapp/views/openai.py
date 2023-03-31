b0f00b4600a260181956d3285c0cb95d4307864b
daviola <18071455+daviola@users.noreply.github.com>
2023-03-31 17:43:11 -0300

    form part 1 e 2

--- webapp/views/openai.py
+++ webapp/views/openai.py
@@ -1,12 +1,36 @@
 from django.shortcuts import render
 
 # Create your views here.
+linguagens = [
+    'c',
+    'clike',
+    'cpp',
+    'csharp',
+    'css',
+    'csv',
+    'django',
+    'go',
+    'graphql',
+    'java',
+    'javascript',
+    'markup',
+    'markup-templating',
+    'perl',
+    'php',
+    'python',
+    'ruby',
+    'rust',
+    'sql',
+    'xml-docyamlhtml'
+    ]
+
 
 def correcao(request):
     params = {
         "view":{
             "id": "correcao",
             "titulo": "Correção de Código"
-        }
+        },
+        "linguagens": linguagens
     }
     return render(request, "correcao.html", params)
