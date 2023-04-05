8bc8ee0ef8648658de2d40564fe3587b3949e9e1
daviola <18071455+daviola@users.noreply.github.com>
2023-04-05 16:22:19 -0300

    auth intro

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 from django.urls import path
 from webapp.views import openai
+from webapp.views import autenticacao
 
 urlpatterns = [    
     path("correcao", openai.correcao, name="correcao"),
     path("criacao", openai.criacao, name="criacao"),
     path("geral", openai.geral, name="geral"),
+    path("signin", autenticacao.signin, name="signin"),
+    path("signout", autenticacao.signout, name="signout"),
+    path("signup", autenticacao.signup, name="signup"),
 ]
---
+++ webapp/views/autenticacao.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.contrib import messages
+
+def signin(request):
+    pass
+
+def signup(request):
+    pass
+
+def signout(request):
+    pass
