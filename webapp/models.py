3473e0c2ef56ccf437f63b0a2c1276bb8ee1faf7
daviola <18071455+daviola@users.noreply.github.com>
2023-04-06 16:35:49 -0300

    model historico

--- webapp/models.py
+++ webapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Registros(models.Model):
+    choices = [
+        ("criacao","criacao"),
+        ("correcao","correcao"),
+        ("geral","geral"),
+    ]
+    user = models.ForeignKey(User, related_name="registros", on_delete=models.DO_NOTHING)
+    pergunta = models.TextField(max_length=5000)
+    resposta = models.TextField(max_length=5000)
+    linguagem = models.CharField(max_length=50)
+    data = models.DateTimeField(auto_now_add=True, blank=True)
+    tipo = models.CharField(max_length=50, choices=choices, null=True)
