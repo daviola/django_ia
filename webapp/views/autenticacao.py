40003daf2ff41288e146e3a31f579c3700141926
daviola <18071455+daviola@users.noreply.github.com>
2023-04-06 10:45:55 -0300

    login logout e navbar

--- webapp/views/autenticacao.py
+++ webapp/views/autenticacao.py
@@ -1,11 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 
 def signin(request):
-    pass
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "Você logou com sucesso")
+            return redirect("correcao")
+        else:
+            messages.success(request, "Falha ao efetuar login")
+            return redirect("correcao")
+    return render(request, "correcao.html")
 
-def signup(request):
-    pass
 
 def signout(request):
+    logout(request)
+    messages.success(request, "Você efetuou logout")
+    return redirect("correcao")
+
+def signup(request):
     pass
